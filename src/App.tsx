ffb1e40c21233a3710a0450614ba15fb25cb2e02
Alec <tellezalec@gmail.com>
2025-09-14 23:46:25 -0400

    added scanner again3

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import Scanner from "./pages/Scanner"
+import Scanner from "./pages/Scanner";
 
 const queryClient = new QueryClient();
 //const basename = import.meta.env.BASE_URL.replace(/\/$/, ""); // '/NUSEC/' -> '/NUSEC'
