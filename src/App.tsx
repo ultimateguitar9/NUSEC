6f6eaeab6727871b71cdbf23540e05e14d8c38df
Alec <tellezalec@gmail.com>
2025-09-14 23:40:32 -0400

    added scanner again2

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/supercoolscanner" element={<Scanner/>} />
+          <Route path="/supercoolscanner" element={<Scanner />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
