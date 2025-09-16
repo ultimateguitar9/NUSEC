f91c3362c2d7787fd9a5f7c85a6ab4b67cfb2853
jalazer <j.almanzar@northeastern.edu>
2025-09-15 22:49:30 -0400

    Added a test page route!

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Scanner from "./pages/Scanner";
+import Test from "./pages/Test";
 
 const queryClient = new QueryClient();
 //const basename = import.meta.env.BASE_URL.replace(/\/$/, ""); // '/NUSEC/' -> '/NUSEC'
@@ -21,6 +22,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/supercoolscanner" element={<Scanner />} />
           <Route path="*" element={<NotFound />} />
+          <Route path="/test" element={<Test />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
---
+++ src/pages/Test.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Test() {
+    return (
+        <>
+            <h1 className="items-center justify-center">Hello World!</h1>
+        </>
+    )
+}
